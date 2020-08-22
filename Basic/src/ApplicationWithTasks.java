d1df94bf6b54cd418af595f4c154d4624bce6f79
xFaabricio <fabricio_oliveira1@hotmail.com>
2020-08-22 14:45:56 -0300

    initial Commit

---
+++ Basic/src/ApplicationWithTasks.java
@@ -0,0 +1,62 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ApplicationWithTasks {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+			
+//		String hostName = "localhost";
+//		String dataBaseName = "SIQ_JUN_WEB";
+//		String dataBasePrefix = "jdbc:sqlserver:/";
+//		String databasePort = "1433";
+		
+		/**
+		 * Exemplo Conexão com SQLSERVER
+		 **/
+		
+		// Informar as configurações do banco de dados
+		
+		String userName = "sa";
+		String password = "lsalles";
+		String jdbcDriver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+
+		String url = "jdbc:sqlserver://localhost:1433;DatabaseName=SIQ_JUN_WEB";
+		
+		// Passando as informações para o construtor
+		
+//		ConnectionDB connectionDB = new ConnectionDB(hostName, userName, password, jdbcDriver, dataBaseName, dataBasePrefix, databasePort);		
+		ConnectionDB connectionDB = new ConnectionDB(userName, password, url, jdbcDriver);
+		
+		//Simples teste para ver se foi realizada a conexão
+		connectionDB.connectionTest();
+		
+		// Realizar a conexão com o banco de dados
+		Connection connectionSQLServer = connectionDB.getConnection();
+		connectionSQLServer.close();
+		
+		
+		/**
+		 * Exemplo Conexão com Oracle
+		 **/
+				
+		// Informar as configurações do banco de dados
+
+		userName = "SIQ_DANIEL";
+		password = "SIQ_DANIEL";
+		jdbcDriver = "oracle.jdbc.driver.OracleDriver";
+		url = "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=ORASIQ)(SERVER=DEDICATED)))";
+		
+		connectionDB = new ConnectionDB(userName, password, url, jdbcDriver);
+		
+		//Simples teste para ver se foi realizada a conexão
+		connectionDB.connectionTest();
+		
+		// Realizar a conexão com o banco de dados
+		Connection connectionOracle = connectionDB.getConnection();
+		connectionOracle.close();
+		
+	}
+
+}
---
+++ Basic/src/ConnectionDB.java
@@ -0,0 +1,188 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+
+	private Connection connection = null;
+	
+	private String hostName = null;
+	private String userName = null;
+	private String password = null;
+	private String url = null;
+	private String jdbcDriver = null;
+	private String dataBaseName = null;
+	private String dataBasePrefix = null;
+	private String dataBasePort = null;
+	
+	
+	public ConnectionDB(String hostName, String userName, String password, String jdbcDriver,
+			String dataBaseName, String dataBasePrefix, String dataBasePort) {
+		super();
+		this.hostName = hostName;
+		this.userName = userName;
+		this.password = password;
+		this.jdbcDriver = jdbcDriver;
+		this.dataBaseName = dataBaseName;
+		this.dataBasePrefix = dataBasePrefix;
+		this.dataBasePort = dataBasePort;
+		
+		this.url = dataBasePrefix + hostName + ":"+dataBasePort+"/" + dataBaseName + "/";
+	}
+	
+	
+
+	public ConnectionDB(String userName, String password, String url, String jdbcDriver) {
+		super();
+		this.userName = userName;
+		this.password = password;
+		this.url = url;
+		this.jdbcDriver = jdbcDriver;
+	}
+
+	public void closeConnection() {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				//TODO: use um sistema de log apropriado.
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public Connection getConnection() {
+		
+		try {
+			
+			if(connection == null) {
+				Class.forName(this.jdbcDriver);
+				connection = DriverManager.getConnection(this.url, this.userName, this.password);				
+			}else if(connection.isClosed()) {
+				connection = null;
+				return getConnection();
+			}			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		return connection;
+	}
+
+	
+	public void connectionTest() {
+				
+		System.out.println("Identificando Banco de Dados...");
+		
+		if(jdbcDriver.equals("oracle.jdbc.driver.OracleDriver")) {
+			System.out.println("Banco: Oracle");
+		}else if(jdbcDriver.equals("com.microsoft.sqlserver.jdbc.SQLServerDriver")) {
+			System.out.println("Banco: SQLServer");
+		}else if(jdbcDriver.equals("")) {
+			System.out.println("Banco: MySQL");
+		}else {
+			System.out.println("Banco não identificado");
+		}
+		
+		try {
+			if(connection.isValid(20)) {
+				System.out.println("Conexão realizada com sucesso !!");
+			}
+		} catch (SQLException e) {
+			// TODO: handle exception
+			System.out.println("Conexão malsucedida !!");
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+
+
+	public String getHostName() {
+		return hostName;
+	}
+
+
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getUrl() {
+		return url;
+	}
+
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+
+	public String getJdbcDriver() {
+		return jdbcDriver;
+	}
+
+
+	public void setJdbcDriver(String jdbcDriver) {
+		this.jdbcDriver = jdbcDriver;
+	}
+
+
+	public String getDataBaseName() {
+		return dataBaseName;
+	}
+
+
+	public void setDataBaseName(String dataBaseName) {
+		this.dataBaseName = dataBaseName;
+	}
+
+
+	public String getDataBasePrefix() {
+		return dataBasePrefix;
+	}
+
+
+	public void setDataBasePrefix(String dataBasePrefix) {
+		this.dataBasePrefix = dataBasePrefix;
+	}
+
+
+	public String getDatabasePort() {
+		return dataBasePort;
+	}
+
+
+	public void setDatabasePort(String databasePort) {
+		this.dataBasePort = databasePort;
+	}
+	
+	
+}
